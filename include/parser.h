f7f9c7365fba685ff20368dfe4504cc4341105fc
wrylynx <proteanone@protonmail.com>
2019-05-28 08:30:13 -0400

    refactored init function

--- include/parser.h
+++ include/parser.h
@@ -181,25 +181,3 @@ void run_command()
 	} while(status);
 }
 
-void init_shell(int argc, char* argv[])
-{
-	if (argc != 1)
-	{
-		if (strncmp(argv[1], "--version", strlen(argv[1])) == 0)
-		{
-			printf("shREWD version 1.0.0\n");
-			exit(EXIT_SUCCESS);
-		}	
-		else if (strncmp(argv[1], "--help", strlen(argv[1])) == 0)
-		{
-			call_help(argv);
-			exit(EXIT_SUCCESS);
-		}	
-	}
-	else
-	{	
-		system("clear");
-		run_command();
-	}
-}
-
--- src/shrewd.c
+++ src/shrewd.c
@@ -1,5 +1,27 @@
 #include "parser.h"
 
+void init_shell(int argc, char* argv[])
+{
+	if (argc != 1)
+	{
+		if (strncmp(argv[1], "--version", strlen(argv[1])) == 0)
+		{
+			printf("shREWD version 1.0.0\n");
+			exit(EXIT_SUCCESS);
+		}	
+		else if (strncmp(argv[1], "--help", strlen(argv[1])) == 0)
+		{
+			call_help(argv);
+			exit(EXIT_SUCCESS);
+		}	
+	}
+	else
+	{	
+		system("clear");
+		run_command();
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	init_shell(argc, argv);
