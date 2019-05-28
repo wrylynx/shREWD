437f304dab5e1ddf4dd840712e91ec0e94d16957
wrylynx <proteanone@protonmail.com>
2019-05-28 08:26:04 -0400

    refactor definitions

---
+++ include/defs.h
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFSIZE 1024
+#define TRUE 1
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
+#define TOKEN_DELIM "\t\r\n\a"
+#define TOKEN_SIZE 64
+
